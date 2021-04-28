88f0c18a717669f4370cf292ed9d55689fa46bc1
Greg0109 <greg.rabago@gmail.com>
2021-04-28 16:55:51 +0200

    Add nav bar and simple links

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  devise_scope :user do
+    get '/users/sign_out' => 'devise/sessions#destroy'
+  end
   root 'home#index'
 end
