cd39a031f0dc9ae5441378cc860785a012f78123
Greg0109 <greg.rabago@gmail.com>
2021-04-28 16:12:50 +0200

    Changes to model

--- app/models/expense.rb
+++ app/models/expense.rb
@@ -1,6 +1,6 @@
 class Expense < ApplicationRecord
-    validates_presence_of :name, presence: true
-    validates_presence_of :amount, presence: true
+    validates :name, presence: true
+    validates :amount, presence: true
 
     belongs_to :user, class_name: 'User'
     has_one :type, through: :user, class_name: 'Type', source: :types, dependent: :destroy
--- app/models/type.rb
+++ app/models/type.rb
@@ -1,6 +1,5 @@
 class Type < ApplicationRecord
-    validates_presence_of :name, presence: true, uniqueness: true
-    validates_uniqueness_of :name
+    validates :name, presence: true, uniqueness: true
 
     belongs_to :user, class_name: 'User', foreign_key: 'user_id'
     has_many :expenses, class_name: 'Expense', foreign_key: 'type_id', dependent: :destroy
