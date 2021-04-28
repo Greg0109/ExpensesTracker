b089f974f9a95f8762f2451f3b5da45ac82a54a0
Greg0109 <greg.rabago@gmail.com>
2021-04-28 16:24:04 +0200

    Add icon and date to types table

--- db/migrate/20210428094016_create_types.rb
+++ db/migrate/20210428094016_create_types.rb
@@ -2,6 +2,8 @@ class CreateTypes < ActiveRecord::Migration[6.1]
   def change
     create_table :types do |t|
       t.string :name
+      t.datetime :date
+      t.string :icon
 
       t.timestamps
     end
