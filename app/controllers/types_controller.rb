6e5e0b23db213e45a7f8e4b9a18333544ec2d193
Greg0109 <greg.rabago@gmail.com>
2021-04-29 00:00:37 +0200

    Add type and type index method

---
+++ app/controllers/types_controller.rb
@@ -0,0 +1,26 @@
+class TypesController < ApplicationController
+  def new
+    @type = Type.new
+  end
+
+  def index
+    @types = Type.where(:user_id => current_user.id)
+  end
+  
+  def create
+    @type = current_user.types.build(type_params)
+
+    respond_to do |format|
+      if @type.save
+        format.html { redirect_to root_path, notice: 'type was successfully created.' }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  private
+  def type_params
+    params.require(:type).permit(:name, :user_id)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :types
   resources :expenses
   devise_for :users
   devise_scope :user do
