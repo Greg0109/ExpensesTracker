06d4ca9a3c9948ea81e377d1e020f349ffd13107
Greg0109 <greg.rabago@gmail.com>
2021-04-28 11:47:19 +0200

    Edit expense, type and user model

--- app/models/type.rb
+++ app/models/type.rb
@@ -1,2 +1,7 @@
 class Type < ApplicationRecord
+    validates_presence_of :name, presence: true, uniqueness: true
+    validates_uniqueness_of :name
+
+    belongs_to :user, class_name: 'User', foreign_key: 'user_id'
+    has_many :expenses, class_name: 'Expense', foreign_key: 'type_id', dependent: :destroy
 end
