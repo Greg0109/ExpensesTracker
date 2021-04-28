2ece231271f8ce7be0196fd223f8f37e75717060
Greg0109 <greg.rabago@gmail.com>
2021-04-28 12:13:06 +0200

    Add name to user and faker gem

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+User.create(name:'Greg', email:'greg.rabago@gmail.com', password:'123456', password_confirmation:'123456')
