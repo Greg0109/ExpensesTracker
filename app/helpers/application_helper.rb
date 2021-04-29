f48da425180984b7fefa8a2d51719ea7bd93fbdf
Greg0109 <greg.rabago@gmail.com>
2021-04-29 11:03:48 +0200

    Add CSS style to all classes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,10 @@
 module ApplicationHelper
     def get_type_icon(type_id)
-        type_name = Type.where(:id => type_id)
-        if type_name.first.icon.present?
-            image_tag(type_name.first.icon, class: 'w-1/5 md:w-2/6')
+        type_name = Type.find_by(:id => type_id)
+        if type_name.present? && type_name.icon.present?
+            image_tag(type_name.icon, class: 'w-1/5 md:w-2/6')
         else
-            'No Icon'
+            image_tag('logo.png', class: 'w-1/5 md:w-2/6')
         end
     end
 end
