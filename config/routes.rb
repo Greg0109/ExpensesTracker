d786d9c80df5d1c76613338082f636396be91992
Greg0109 <greg.rabago@gmail.com>
2021-05-01 23:38:20 +0200

    Allow only specific routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  resources :types
-  resources :expenses
+  resources :types, only: [:new, :show, :index, :create]
+  resources :expenses, only: [:new, :show, :index, :create]
   devise_for :users
   devise_scope :user do
     get '/users/sign_out' => 'devise/sessions#destroy'
